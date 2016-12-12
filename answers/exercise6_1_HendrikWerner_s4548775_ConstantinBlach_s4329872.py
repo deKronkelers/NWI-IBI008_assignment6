f11ae6ad3cf9b5ffda0fe0653f5006a7ce1391f4
Hendrikto <hendrik.to@gmail.com>
2016-12-12 19:08:11 +0100

    Add answer stubs.

---
+++ answers/exercise6_1_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
@@ -0,0 +1,6 @@
+# author: Hendrik Werner s4549775
+# author: Constantin Blach s4329872
+
+# exercise 1.1
+# exercise 1.2
+# exercise 1.3
---
+++ answers/exercise6_2_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
@@ -0,0 +1,7 @@
+# author: Hendrik Werner s4549775
+# author: Constantin Blach s4329872
+
+# exercise 2.1
+# exercise 2.2
+# exercise 2.3
+# exercise 2.4
