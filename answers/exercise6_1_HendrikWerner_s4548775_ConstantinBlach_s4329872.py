a50caac9ccc8ad289a11eab5995cdc6300ad2bed
Hendrikto <hendrik.to@gmail.com>
2016-12-16 14:59:22 +0100

    Load synthetic data sets.

--- answers/exercise6_1_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
+++ answers/exercise6_1_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
@@ -1,6 +1,15 @@
 # author: Hendrik Werner s4549775
 # author: Constantin Blach s4329872
 
+from scipy.io import loadmat
+
 # exercise 1.1
+synths = [
+    loadmat("./data/synth1.mat")
+    , loadmat("./data/synth2.mat")
+    , loadmat("./data/synth3.mat")
+    , loadmat("./data/synth4.mat")
+]
+
 # exercise 1.2
 # exercise 1.3
