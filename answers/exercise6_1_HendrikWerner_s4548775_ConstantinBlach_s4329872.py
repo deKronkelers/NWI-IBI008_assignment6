45bb5fc0c42023f879c0bf55ec663db46b69cb5f
Hendrikto <hendrik.to@gmail.com>
2016-12-16 14:59:59 +0100

    Draw scatterplots of the data.

--- answers/exercise6_1_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
+++ answers/exercise6_1_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
@@ -1,6 +1,7 @@
 # author: Hendrik Werner s4549775
 # author: Constantin Blach s4329872
 
+import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
 # exercise 1.1
@@ -10,6 +11,15 @@ synths = [
     , loadmat("./data/synth3.mat")
     , loadmat("./data/synth4.mat")
 ]
+
+for i, synth in enumerate(synths):
+    # scatterplot
+    X = synth["X"]
+    plt.scatter(X[:, 0], X[:, 1])
+    plt.title("Synth {}".format(i + 1))
+    plt.xlabel("Attribute 1")
+    plt.ylabel("Attribute 2")
+    plt.show()
 
 # exercise 1.2
 # exercise 1.3
