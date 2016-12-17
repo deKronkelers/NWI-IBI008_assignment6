63cfd2510051949a0a3d66ab17e8838b6de699b4
Hendrikto <hendrik.to@gmail.com>
2016-12-17 17:43:16 +0100

    Draw a scatter plot of the xor data set.

--- answers/exercise6_2_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
+++ answers/exercise6_2_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
@@ -1,7 +1,25 @@
 # author: Hendrik Werner s4549775
 # author: Constantin Blach s4329872
 
+import neurolab
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
 # exercise 2.1
+xor = loadmat("./data/xor.mat")
+X = xor["X"]
+y = xor["y"]
+X_y0 = X[(y == 0).ravel()]
+X_y1 = X[(y == 1).ravel()]
+
+plt.scatter(X_y0[:, 0], X_y0[:, 1], color="red", label="Class 0")
+plt.scatter(X_y1[:, 0], X_y1[:, 1], color="blue", label="Class 1")
+plt.title("Xor Scatterplot")
+plt.xlabel("Attribute 1")
+plt.ylabel("Attribute 2")
+plt.legend(loc="lower left")
+plt.show()
+
 # exercise 2.2
 # exercise 2.3
 # exercise 2.4
