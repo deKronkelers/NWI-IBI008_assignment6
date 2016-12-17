886aa38866b589847f80b4917c18627c5056d68e
Hendrikto <hendrik.to@gmail.com>
2016-12-17 18:42:08 +0100

    Refactor into function.

--- answers/exercise6_2_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
+++ answers/exercise6_2_HendrikWerner_s4548775_ConstantinBlach_s4329872.py
@@ -9,15 +9,20 @@ from scipy.io import loadmat
 xor = loadmat("./data/xor.mat")
 X = xor["X"]
 y = xor["y"]
-X_y0 = X[(y == 0).ravel()]
-X_y1 = X[(y == 1).ravel()]
 
-plt.scatter(X_y0[:, 0], X_y0[:, 1], color="red", label="Class 0")
-plt.scatter(X_y1[:, 0], X_y1[:, 1], color="blue", label="Class 1")
+
+def scatter_plot_by_class(X: np.ndarray) -> None:
+    X_y0 = X[(y == 0).ravel()]
+    X_y1 = X[(y == 1).ravel()]
+    plt.scatter(X_y0[:, 0], X_y0[:, 1], color="red", label="Class 0")
+    plt.scatter(X_y1[:, 0], X_y1[:, 1], color="blue", label="Class 1")
+    plt.xlabel("Attribute 1")
+    plt.ylabel("Attribute 2")
+    plt.legend(loc="lower left")
+
+
+scatter_plot_by_class(X)
 plt.title("Xor Scatterplot")
-plt.xlabel("Attribute 1")
-plt.ylabel("Attribute 2")
-plt.legend(loc="lower left")
 # plt.savefig("assignment6_2_scatter_plot_xor.pdf")
 plt.show()
 
